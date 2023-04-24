f3be1389378c1b7569a529feac7af18e9b337ea0
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-04-24 15:52:17 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <Novice.h>
+#include"MyMatrix4x4.h"
+#include"MatrixScreenPrintf.h"
+#include"MyVector3.h"
+#include"VectorScreenPrintf.h"
+
+const char kWindowTitle[] = "学籍番号";
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	MyVector3 translate{ 4.1f,2.6f,0.8f };
+	MyVector3 scale{ 1.5f,5.2f,7.3f };
+	MyMatrix4x4 translateMatrix = MyMatrix4x4::MakeTranslateMatrix(translate);
+	MyMatrix4x4 scaleMatrix = MyMatrix4x4::MakeScaleMatrix(scale);
+	MyVector3 point{ 2.3f,3.8f,1.4f };
+	MyMatrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
+	MyVector3 transformed = MyMatrix4x4::Transform(point, transformMatrix);
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+		
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, kRowHeight * 2, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 7, scaleMatrix, "scaleMatrix");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
